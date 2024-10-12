2253a9f97573aad6b7aabf02af035055e0388b5e
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-10-12 20:48:50 +0200

    managing secret key

--- CateringDBSystem/settings.py
+++ CateringDBSystem/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-9^y^hjv8d83!qg+tc9j9bn5l=4il#)+a@ftdoi6fe5xjifh!ab')
+load_dotenv()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
