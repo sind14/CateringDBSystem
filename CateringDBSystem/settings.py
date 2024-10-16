5212389de7e32a3d9b9e14ed79246f8f91f193c9
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-10-16 14:52:30 +0200

    remove insecure django secret key for deployment

--- CateringDBSystem/settings.py
+++ CateringDBSystem/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = 'RENDER' not in os.environ
 
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
