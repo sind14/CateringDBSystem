b7a978e1a825b867c39af176854e4cc189c97a97
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-10-16 15:53:59 +0200

    add cateringdbsystem.onrender.com to ALLOWED_HOSTS and change DEBUG

--- CateringDBSystem/settings.py
+++ CateringDBSystem/settings.py
@@ -25,10 +25,9 @@ load_dotenv()
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "cateringdbsystem.onrender.com"]
 
 
 # Application definition
